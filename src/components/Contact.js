e2b707abf65961ab87c51189392b9c08aff7d8f1
Dylan Andrew RAZAFINTSALAMA <diix.andrew@gmail.com>
2023-07-21 02:25:04 +0200

    contact button

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -18,7 +18,7 @@ export default function Contact() {
               <Input size="lg" color="indigo" label="Email" className="message"/>
               <Textarea color="indigo" label="Message" className="message"/>
             </div>
-            <Button className="mt-6" fullWidth>
+            <Button color="red" variant="outlined" className="flew m-auto rounded-full mt-6">
               Envoyer
             </Button>
           </form>
