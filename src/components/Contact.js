418f1e5498118214ac1bcc64ce736bf4b8211bb0
Dylan Andrew RAZAFINTSALAMA <diix.andrew@gmail.com>
2023-07-21 02:19:35 +0200

    contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,19 +4,19 @@ import { Card, Input, Button, Typography, Textarea } from "@material-tailwind/re
 
 export default function Contact() {
   return (
-    <FullpageSection>
-      <div class="flex items-center justify-center relative h-full w-full">
-        <Card color="transparent" shadow={false}>
+    <FullpageSection className="bg-[url(../public/images/Contact.jpg)] bg-cover">
+      <div className="flex items-center justify-center relative h-full w-full bg-black/75">
+        <Card color="transparent" shadow={true}>
           <div className="text-center">
-            <Typography variant="h2" color="blue-gray">
+            <Typography variant="h2" color="white">
               Contactez-moi
             </Typography>
           </div>
           <form className="mt-8 mb-2 w-80 max-w-screen-lg sm:w-96">
             <div className="mb-4 flex flex-col gap-6">
-              <Input size="lg" label="Name" />
-              <Input size="lg" label="Email" />
-              <Textarea label="Message" />
+              <Input size="lg" color="indigo" label="Name" className="message"/>
+              <Input size="lg" color="indigo" label="Email" className="message"/>
+              <Textarea color="indigo" label="Message" className="message"/>
             </div>
             <Button className="mt-6" fullWidth>
               Envoyer
